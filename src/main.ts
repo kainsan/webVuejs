c12d128fc981e97e9fba5cfc66618fd23ce1fdce
Khanh <khanhtruong326@gmail.com>
2023-06-15 17:41:14 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core CSS
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css"; //icons
+
+import App from './App.vue'
+import router from './router'
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue);
+app.mount('#app')
